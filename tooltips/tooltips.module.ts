f398e75b1450cfd2f80cfbdaa37841f0e510b9f2
Carlos Abarca Vega <105996405+Compuelec@users.noreply.github.com>
2023-04-21 11:44:46 -0400

    Add files via upload

---
+++ tooltips/tooltips.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TooltipsComponent } from './tooltips.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  declarations: [TooltipsComponent],
+  exports: [
+    TooltipsComponent,
+  ],
+})
+export class TooltipsModule { }
